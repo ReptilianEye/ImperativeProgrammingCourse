255a14068497e0089b69e76bfc0c7083f2455030
Piotr Rzadkowski <81797728+ReptilianEye@users.noreply.github.com>
2023-04-01 13:07:20 +0000

    done with cs

---
+++ zad4/finding_identifiersV2.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define IN_LINE_COMMENT 1
+#define IN_BLOCK_COMMENT 2
+#define IN_STRING 4
+#define IN_ID 8
+
+#define MAX_ID_LEN 64
+#define MAX_IDS 1024
+
+#define TEST 1 // 1 - dla testowania,  0 - dla automatycznej oceny
+
+int index_cmp(const void *, const void *);
+int cmp(const void *, const void *);
+
+char tab[MAX_IDS][MAX_ID_LEN];
+
+char *keywords[] = {
+    "auto", "break", "case", "char",
+    "const", "continue", "default", "do",
+    "double", "else", "enum", "extern",
+    "float", "for", "goto", "if",
+    "int", "long", "register", "return",
+    "short", "signed", "sizeof", "static",
+    "struct", "switch", "typedef", "union",
+    "unsigned", "void", "volatile", "while"};
+
+int find_idents(FILE *stream)
+{
+    int next_free = 0;
+    char prev = '\n', c;
+    char **res;
+    int block = 0;
+    // char *word = (char *)malloc(MAX_ID_LEN * sizeof(char));
+
+    while ((c = getc(stream)) != EOF)
+    {
+        if (!block)
+        {
+            if (prev == '/' && c == '*')
+            {
+                block = 1;
+            }
+            else if (prev == '/' && c == '/')
+                while (c != '\n')
+                {
+                    c = getc(stream);
+                }
+            else if (isblank(prev) || prev == '\n')
+            {
+                char rob[MAX_ID_LEN];
+                for (int i = 0; !isblank(c) && c != '/'; c = getc(stream), i++)
+                {
+                    rob[i] = c;
+                    // word[i] = c; // potrzebny lepszy sposob aby to kopiowac (mozliwe ze dodatkowa zmienna)
+                }
+                // strcpy(word,rob);
+                char *word= {"hello"};
+                **res = bsearch(&word, &keywords, sizeof *keywords, sizeof(int), cmp);
+                if (res == NULL)
+                {
+                    **res = bsearch(word, tab, MAX_IDS, sizeof(int), index_cmp);
+                    if (res == NULL)
+                    {
+                        strcpy(tab[next_free], word);
+                        next_free++;
+                    }
+                }
+            }
+        }
+        if (prev == '*' && c == '/')
+        {
+            block = 0;
+        }
+        prev = c;
+    }
+}
+
+int cmp(const void *first_arg, const void *second_arg)
+{
+    char *a = *(char **)first_arg;
+    char *b = *(char **)second_arg;
+    return strcmp(a, b);
+}
+
+int index_cmp(const void *first_arg, const void *second_arg)
+{
+    int a = *(int *)first_arg;
+    int b = *(int *)second_arg;
+    return strcmp(tab[a], tab[b]);
+}
+
+int main(void)
+{
+    char file_name[40];
+    FILE *stream;
+
+    if (TEST)
+        stream = stdin;
+    else
+    {
+        scanf("%s", file_name);
+        stream = fopen(file_name, "r");
+        if (stream == NULL)
+        {
+            printf("fopen failed\n");
+            return -1;
+        }
+    }
+    printf("%d\n", find_idents(stream));
+    return 0;
+}
